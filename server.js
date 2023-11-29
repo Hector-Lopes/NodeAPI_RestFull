990a8dca465da88feb1f066099e7bfb236707c7f
hector.lopes <hector.lopes@anchieta.br>
2023-11-29 08:20:11 -0300

    feat: install express

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-import { createServer } from 'node:http'
+import { createServer } from "node:http";
 
-const server = createServer ((request,response)=>{
-response.write("ola")
+const server = createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.write("ola");
 
-return response.end()
-})
+  if (request.url === "/User") {
+    return response.end(
+      JSON.stringify({
+        message: "Rota de usuario",
+      })
+    );
+  }
+  if (request.url === "/Product") {
+    return response.end(
+      JSON.stringify({
+        message: "Rota de Produto",
+      })
+    );
+  }
+  return response.end(
+    JSON.stringify({
+      message: "Rota padrão",
+    })
+  );
+});
 
-server.listen(3333)
+server.listen(3334), () => console.log("Servidor rodando na porta 3333");
