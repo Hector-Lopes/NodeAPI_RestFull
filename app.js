09ee7bcae8a04cb79b1dfb6b4931f4b83d25c8ed
hector.lopes <hector.lopes@anchieta.br>
2023-11-30 16:31:10 -0300

    feat: add rest full api and store in a textfile

--- app.js
+++ app.js
@@ -1,11 +1,83 @@
 const express = require("express");
+const { randomUUID } = require("crypto");
+const { request } = require("http");
+const fs = require("fs");
 
 const app = express();
 
-app.get("/primeira-rota", (request, response) => {
-  return response.json({
-    message: "Rota primeira",
+app.use(express.json());
+
+let produtos = [];
+
+fs.readFile("produtos.json", "utf-8", (err, data) => {
+  if (err) {
+  } else {
+    produtos = JSON.parse(data);
+  }
+});
+
+// POST => Inserir Dados
+// GET => Buscar Dados
+// PUT => Alterar Dados
+// DELETE => Remover Dados
+
+//body => Sempre que eu quiser enviar dados para minha aplicação
+//Params => /Produtcs/hector.lopes  ou /Produtcs/233543657
+//Query => /Produtcs?user=hector.lopes ou /Produtcs?id=125437567
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+
+  const product = {
+    name,
+    price,
+    id: randomUUID(),
+  };
+
+  produtos.push(product);
+
+  fs.writeFile("produtos.json", JSON.stringify(produtos), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Inserido com sucesso");
+    }
   });
+
+  return response.json(product);
+});
+
+app.get("/products", (request, response) => {
+  return response.json(produtos);
+});
+app.get("/products/:id", (request, response) => {
+  const { id } = request.params;
+
+  const product_find = produtos.find((product) => product.id === id);
+  return response.json(product_find);
+});
+
+app.put("/products/:id", (request, response) => {
+  const { name, price } = request.body;
+  const { id } = request.params;
+  const product_findindex = produtos.findIndex((product) => product.id === id);
+
+  produtos[product_findindex] = {
+    ...produtos[product_findindex],
+    name,
+    price,
+  };
+
+  return response.json({ message: "Produto Alterado" });
+});
+
+app.delete("/products/:id", (request, response) => {
+  const { id } = request.params;
+  const product_findindex = produtos.findIndex((product) => product.id === id);
+
+  produtos.splice(product_findindex, 1);
+
+  return response.json({ message: "Produto removido" });
 });
 
 app.listen(3333, () => console.log("servidor rodando porta 3333"));
