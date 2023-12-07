c25e96c17ddf5a5aed0ea6e9f4d30556cf9dcd7b
hector.lopes <hector.lopes@anchieta.br>
2023-12-07 11:00:29 -0300

    feat: add routes delete,put

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,6 +3,8 @@ import { randomUUID } from "crypto";
 
 import Select_Products from "../services/Select_Products.js";
 import Post_Products from "../services/Post_Products.js";
+import Update_Products from "../services/Update_Pruducts.js";
+import Delete_Product from "../services/Delete_Products.js";
 
 const ProductRouter = Router();
 
@@ -14,14 +16,33 @@ ProductRouter.get("/", async (request, response) => {
 });
 
 ProductRouter.post("/", async (request, response) => {
+  const { name, price } = request.body;
+
   const Product = {
-    name: "livro2",
-    price: "10",
+    name: name,
+    price: price,
   };
 
   const resultsqlpost = await Post_Products(Product);
 
   return response.json("inserido com sucesso");
+});
+ProductRouter.put("/:id", async (request, response) => {
+  const { id } = request.params;
+  const { name, price } = request.body;
+  const Product = {
+    id: id,
+    name: name,
+    price: price,
+  };
+
+  const resultsqlput = await Update_Products(Product);
+});
+
+ProductRouter.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const resultsqldelete = await Delete_Product(id);
 });
 
 export default ProductRouter;
