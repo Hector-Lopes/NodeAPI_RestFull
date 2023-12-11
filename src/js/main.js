449602003246093b52fd753e5828a44d75138e10
hector.lopes <hector.lopes@anchieta.br>
2023-12-11 16:30:09 -0300

    feat: add functionss put and one select

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,28 @@
-CatchValue = () => {
-  $.ajax({
-    type: "GET",
-    url: "http://localhost:3333/products",
-    data: {},
-    cache: false,
-    complete: (resultsql) => {
-      alert("selecionado com sucesso");
-      console.log(resultsql);
-    },
+CatchValue = async () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3333/products",
+      data: {},
+      cache: false,
+      success: (resultsql) => {
+        resolve(resultsql);
+      },
+    });
+  });
+};
+
+CatchOneValue = async (id) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3333/products/" + id,
+      data: {},
+      cache: false,
+      success: (resultsql) => {
+        resolve(resultsql);
+      },
+    });
   });
 };
 
@@ -29,6 +44,35 @@ PostValue = () => {
 
     error: (resultsql) => {
       console.log("erro");
+    },
+  });
+};
+
+PutValue = async () => {
+  const id = $(".input_id_updt").val();
+  const name = $(".input_name_updt").val();
+  const price = $(".input_price_updt").val();
+  // const Product = await CatchOneValue(id);
+  // if (!Product) {
+  //   console.error("Product not found");
+  //   return;
+  // } else {
+  //   console.log(Product);
+  // }
+
+  // const { name, price } = Product[0];
+
+  console.log(name);
+  console.log(price);
+
+  $.ajax({
+    type: "PUT",
+    url: "http://localhost:3333/products/" + id,
+    data: { name: name, price: price },
+    cache: false,
+    success: (resultsql) => {
+      alert("atualizado com sucesso");
+      console.log(resultsql);
     },
   });
 };
