c19675ea14eeb83fe280b84ccc64489e78ffa95a
hector.lopes <hector.lopes@anchieta.br>
2023-12-12 14:57:13 -0300

    refact: change to return the rowcount instead the sql command

--- src/services/Delete_Products.js
+++ src/services/Delete_Products.js
@@ -1,18 +1,13 @@
 import { sql } from "../database/db.js";
 
 const Delete_Product = async (id) => {
-  sql`
+  const result = await sql`
 delete from products where id=${id}
-returning *
-`.then((e) => {
-    const rowcount = e.length;
-    if (rowcount >= 1) {
-      console.log("Deletado com sucesso");
-    } else {
-      console.log("Falha ao Deletar ");
-    }
-  });
-  return sql;
+returning id
+`;
+  const rowcount = result.length;
+  console.log(rowcount);
+  return rowcount;
 };
 
 export default Delete_Product;
