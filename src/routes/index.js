9162519cc3cb002ac42ac8314fcadccda9d6121d
hector.lopes <hector.lopes@anchieta.br>
2023-12-13 16:28:14 -0300

    feat: add routes of login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import ProductRouter from "./product.routes.js";
-
+import UserRouter from "./user.routes.js";
 const routes = Router();
 
 routes.use("/products", ProductRouter);
+routes.use("/user", UserRouter);
 
 export default routes;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,23 @@
+import { Router, request, response } from "express";
+import jwt from "jsonwebtoken";
+import bodyParser from "body-parser";
+
+import Login_User from "../services/Login_User.js";
+
+const UserRouter = Router();
+
+UserRouter.use(bodyParser.json());
+UserRouter.use(bodyParser.urlencoded({ extended: false }));
+
+UserRouter.get("/", async (request, response) => {
+  const { user, password } = request.query;
+
+  const User = { user: user, password: password };
+
+  const result = await Login_User(User);
+
+  console.log(result);
+  return response.json(result);
+});
+
+export default UserRouter;
